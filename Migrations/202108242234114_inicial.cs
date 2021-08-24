1742a181a519c6d501c27a5f516210f406d9920c
veronica <Vero>
2021-08-24 16:36:24 -0600

    Se habilitó migración (enable-migrations) y se agregó (add-migration)

---
+++ Migrations/202108242234114_inicial.cs
@@ -0,0 +1,29 @@
+﻿namespace MyPracticeCass.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class inicial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Productos",
+                c => new
+                    {
+                        IdProducto = c.Int(nullable: false, identity: true),
+                        CodigoProducto = c.String(nullable: false),
+                        DescripcionProducto = c.String(nullable: false),
+                        CantidadProducto = c.Int(nullable: false),
+                        PrecioProducto = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.IdProducto);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Productos");
+        }
+    }
+}
