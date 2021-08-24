bc8aa86ba22fe2c2ffd33179a95427468a915d03
veronica <Vero>
2021-08-24 16:07:49 -0600

    Se creó la clase Productos.cs en el paquete models

---
+++ Models/Productos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyPracticeCass.Models
+{
+    public class Productos
+    {
+        //Atributos de la clase
+        [Key] //LLave primaria de la tabla
+        public int IdProducto { get; set; }
+        [Required(ErrorMessage ="El campo no puede ser nulo")] 
+        public string CodigoProducto { get; set; }
+        [Required(ErrorMessage = "El campo no puede ser nulo")]
+        public string DescripcionProducto { get; set; }
+        [Required(ErrorMessage = "El campo no puede ser nulo")]
+        public int CantidadProducto { get; set; }
+        [Required(ErrorMessage = "El campo no puede ser nulo")]
+        public double PrecioProducto { get; set; }
+    }
+}
