a0e10334fbb704ef30f6a68d40ce007964b90d16
veronica <Vero>
2021-08-24 20:13:34 -0600

    Se agregó vista "MostrarProductos" y se configuró redirección

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MyPracticeCass
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Productos", action = "MostrarProductos", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPracticeCass.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult MostrarProductos()
+        {
+            return View();
+        }
+    }
+}
