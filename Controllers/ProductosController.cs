b98ef53835bf3b5334491e87e17d9b8d444c2078
veronica <Vero>
2021-08-25 20:11:04 -0600

    Métodos para crear, editar y eliminar productos

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -1,6 +1,7 @@
 ﻿using MyPracticeCass.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -19,5 +20,54 @@ namespace MyPracticeCass.Controllers
             var model = contexto.Productos.ToList();
             return View(model);
         }
+
+
+        public ActionResult CreateOrEditProductos(int idProducto = 0)
+        {
+            if (idProducto > 0)
+            {
+                var model = contexto.Productos.FirstOrDefault(x => x.IdProducto == idProducto);
+                return View(model);
+            }
+            else
+            {
+                var model = new Productos();
+                return View(model);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult CreateOrEditProductos(Productos model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (model.IdProducto != 0)
+                {
+                    contexto.Entry(model).State = EntityState.Modified;
+                    contexto.SaveChanges();
+                }
+                else
+                {
+                    contexto.Entry(model).State = EntityState.Added;
+                    contexto.SaveChanges();
+                }
+
+                return RedirectToAction("MostrarProductos");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult EliminarProductos(int idProducto)
+        {
+            var model = contexto.Productos.FirstOrDefault(x => x.IdProducto == idProducto);
+            contexto.Entry(model).State = EntityState.Deleted;
+            contexto.SaveChanges();
+
+            return RedirectToAction("MostrarProductos");
+        }
+
     }
 }
